bb4d06944e4276dcaf3c404ce89282248040d204
NRamm99 <162293775+NRamm99@users.noreply.github.com>
2025-09-12 18:12:18 +0200

    rettelser

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,15 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int timeOfDay = 1; // 1: dag | 2: aften | 3: nat
-        int trafficDensity = 1; // 1: høj | 2: lav
-        int areaType = 1; // 1: bolig | 2: erhverv | 3: blandet
-        boolean emergency = false;
-        int emergencyCheck = 0;
-        int trafficLight = 1; // 1: grønt || 2: Gult || 3: Rødt
-        boolean entryAllowed = false;
+        int timeOfDay; // 1: dag | 2: aften | 3: nat
+        int trafficDensity; // 1: høj | 2: lav
+        int areaType; // 1: bolig | 2: erhverv | 3: blandet
+        int trafficLight = 0; // 1: grønt || 2: Gult || 3: Rødt || 4: Ingen adgang
+        int emergency;
 
         Scanner input = new Scanner(System.in);
 
-        //Valg af dag
+        // Valg af tid
         System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
         System.out.println("Vælg venligst tid på dagen.");
         System.out.println("1 = Dag");
@@ -22,14 +20,14 @@ public class Main {
         System.out.println("3 = Nat");
         timeOfDay = input.nextInt();
 
-        //Valg af høj eller lav trafik
+        // Valg af høj eller lav trafik
         System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
         System.out.println("Vælg type trafik:");
         System.out.println("1 = Høj trafik");
         System.out.println("2 = Lav trafik");
         trafficDensity = input.nextInt();
 
-        //valg af område
+        // Valg af område
         System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
         System.out.println("Vælg venligst områdetype:");
         System.out.println("1 = Boligområde");
@@ -37,20 +35,16 @@ public class Main {
         System.out.println("3 = Blandet");
         areaType = input.nextInt();
 
-        //Emergency?
+        // Emergency?
         System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - -");
         System.out.println("Er det en nødsituation?");
         System.out.println("1 = Ja");
         System.out.println("2 = Nej");
-        emergencyCheck = input.nextInt();
-        if (emergencyCheck == 1) {
-            emergency = true;
-        } else {
-            emergency = false;
-        }
+        emergency = input.nextInt();
+
 
         // Grønt lys
-        if (timeOfDay == 1 && trafficDensity == 2 || emergency) {
+        if (timeOfDay == 1 && trafficDensity == 2 || emergency == 1) {
             trafficLight = 1;
         }
 
@@ -63,43 +57,49 @@ public class Main {
         else if (timeOfDay == 3 && trafficDensity == 1 || areaType == 1 && trafficDensity == 1) {
             trafficLight = 3;
         } else {
-            System.out.println("END OF CODE - FEJL (INGEN IF STATEMENT MØDT)");
+            System.out.println("FEJL: Ingen if statement for trafficlight var mødt.");
         }
 
-// Betingelser for adgang
-        if (areaType == 2 || areaType == 3 && trafficDensity == 2) {
-            entryAllowed = true;
-        }else if(areaType == 1 && timeOfDay == 3){
-            entryAllowed = false;
+        // Natlukning check
+        if (areaType == 1 && timeOfDay == 3 && emergency == 0) {
+            trafficLight = 4;
         }
 
 
         //  Final report print
-        // GRØNT LYS
-        if (trafficLight == 1) {
-            System.out.println("GRØNT LYS");
-            System.out.println("(⬛)");
-            System.out.println("(⬛)");
-            System.out.println("(\uD83D\uDFE9)");
-        }
-        // GULT LYS
-        else if (trafficLight == 2) {
-            System.out.println("GULT LYS");
-            System.out.println("(⬛)");
-            System.out.println("(\uD83D\uDFE8)");
-            System.out.println("(⬛)");
-        }
-        // RØDT LYS
-        else if (trafficLight == 3) {
-            System.out.println("RØDT LYS");
-            System.out.println("(\uD83D\uDFE5)");
-            System.out.println("(⬛)");
-            System.out.println("(⬛)");
+        switch (trafficLight) {
+            // GRØNT LYS
+            case 1:
+                System.out.println("GRØNT LYS");
+                System.out.println("(⬛)");
+                System.out.println("(⬛)");
+                System.out.println("(\uD83D\uDFE9)");
+                break;
+            // GULT LYS
+            case 2:
+                System.out.println("GULT LYS");
+                System.out.println("(⬛)");
+                System.out.println("(\uD83D\uDFE8)");
+                System.out.println("(⬛)");
+                break;
+            // RØDT LYS
+            case 3:
+                System.out.println("RØDT LYS");
+                System.out.println("(\uD83D\uDFE5)");
+                System.out.println("(⬛)");
+                System.out.println("(⬛)");
+                break;
+            // NO ENTRY
+            case 4:
+                System.out.println("⛔ INGEN ADGANG ⛔");
+                System.out.println("\uD83C\uDF03 Natlukning i boligområde. \uD83C\uDF03");
+                break;
+            // FEJLKODE
+            default:
+                System.out.println("INTET LYS - FEJL I KODE!!!");
+                break;
 
         }
-        // FEJLKODE
-        else {
-            System.out.println("INTET LYS - FEJL I KODE!!!");
-        }
+
     }
 }
